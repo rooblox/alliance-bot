e081588b568c093d12072264f6022311e06b7f55
rooblox <34076536+rooblox@users.noreply.github.com>
2025-12-22 15:34:17 -0500

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,19 +11,9 @@ commands.push(
         .setName('status')
         .setDescription('Change the bot status')
         .addStringOption(opt =>
-            opt.setName('type')
-                .setDescription('Select the status type')
-                .setRequired(true)
-                .addChoices(
-                    { name: 'Playing', value: 'PLAYING' },
-                    { name: 'Watching', value: 'WATCHING' },
-                    { name: 'Listening', value: 'LISTENING' },
-                    { name: 'Competing', value: 'COMPETING' }
-                ))
-        .addStringOption(opt =>
-            opt.setName('text')
-                .setDescription('The status text to display')
-                .setRequired(true))
+            opt.setName('status')
+               .setDescription('Status text to show (Playing, Watching, etc.)')
+               .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .toJSON()
 );
@@ -34,15 +24,11 @@ commands.push(
 commands.push(
     new SlashCommandBuilder()
         .setName('dm')
-        .setDescription('Send a direct message to a user')
+        .setDescription('Send a DM via the bot')
         .addUserOption(opt =>
-            opt.setName('user')
-                .setDescription('The user to DM')
-                .setRequired(true))
+            opt.setName('user').setDescription('User to DM').setRequired(true))
         .addStringOption(opt =>
-            opt.setName('message')
-                .setDescription('The message content')
-                .setRequired(true))
+            opt.setName('message').setDescription('Message to send').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .toJSON()
 );
@@ -53,36 +39,35 @@ commands.push(
 commands.push(
     new SlashCommandBuilder()
         .setName('alliance')
-        .setDescription('Manage alliances')
+        .setDescription('Alliance management')
         .addSubcommand(sub =>
             sub.setName('add')
-                .setDescription('Add a new alliance')
-                .addStringOption(o => o.setName('group').setDescription('Name of the alliance group').setRequired(true))
-                .addStringOption(o => o.setName('our_reps').setDescription('Our representatives in Discord').setRequired(true))
-                .addStringOption(o => o.setName('their_reps').setDescription('Their representatives in Discord').setRequired(true))
-                .addStringOption(o => o.setName('discord').setDescription('Discord invite link for the alliance').setRequired(true))
-                .addStringOption(o => o.setName('roblox').setDescription('Roblox group link for the alliance').setRequired(true))
+               .setDescription('Add a new alliance')
+               .addStringOption(o => o.setName('group').setDescription('Alliance group name').setRequired(true))
+               .addStringOption(o => o.setName('our_reps').setDescription('Our representatives').setRequired(true))
+               .addStringOption(o => o.setName('their_reps').setDescription('Their representatives').setRequired(true))
+               .addStringOption(o => o.setName('discord').setDescription('Discord link').setRequired(true))
+               .addStringOption(o => o.setName('roblox').setDescription('Roblox link').setRequired(true))
         )
         .addSubcommand(sub =>
             sub.setName('list')
-                .setDescription('List all current alliances')
+               .setDescription('List all alliances')
         )
         .addSubcommand(sub =>
             sub.setName('remove')
-                .setDescription('Remove an existing alliance')
-                .addStringOption(o => o.setName('group').setDescription('Name of the alliance to remove').setRequired(true))
+               .setDescription('Remove an alliance')
+               .addStringOption(o => o.setName('group').setDescription('Alliance group to remove').setRequired(true))
         )
         .addSubcommand(sub =>
             sub.setName('edit')
-                .setDescription('Edit an existing alliance')
-                .addStringOption(o => o.setName('group').setDescription('Name of the alliance to edit').setRequired(true))
-                .addStringOption(o => o.setName('new_group').setDescription('New alliance name'))
-                .addStringOption(o => o.setName('our_reps').setDescription('New our representatives'))
-                .addStringOption(o => o.setName('their_reps').setDescription('New their representatives'))
-                .addStringOption(o => o.setName('discord').setDescription('New Discord link'))
-                .addStringOption(o => o.setName('roblox').setDescription('New Roblox link'))
+               .setDescription('Edit an existing alliance')
+               .addStringOption(o => o.setName('group').setDescription('Alliance group to edit').setRequired(true))
+               .addStringOption(o => o.setName('new_group').setDescription('New group name'))
+               .addStringOption(o => o.setName('our_reps').setDescription('New our reps'))
+               .addStringOption(o => o.setName('their_reps').setDescription('New their reps'))
+               .addStringOption(o => o.setName('discord').setDescription('New Discord link'))
+               .addStringOption(o => o.setName('roblox').setDescription('New Roblox link'))
         )
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .toJSON()
 );
 
@@ -92,28 +77,28 @@ commands.push(
 commands.push(
     new SlashCommandBuilder()
         .setName('staff')
-        .setDescription('Staff management commands')
+        .setDescription('Staff management')
         .addSubcommand(sub =>
             sub.setName('discipline')
-                .setDescription('Discipline a staff member (strike, kick, terminate)')
-                .addUserOption(o => o.setName('member').setDescription('The staff member to discipline').setRequired(true))
-                .addStringOption(o =>
-                    o.setName('action')
-                        .setDescription('Action to perform')
-                        .setRequired(true)
-                        .addChoices(
-                            { name: 'Strike', value: 'strike' },
-                            { name: 'Kick', value: 'kick' },
-                            { name: 'Terminate', value: 'terminate' }
-                        ))
-                .addStringOption(o => o.setName('reason').setDescription('Reason for the action').setRequired(true))
+               .setDescription('Strike, remove, or kick a member')
+               .addUserOption(o => o.setName('member').setDescription('Member to act on').setRequired(true))
+               .addStringOption(o =>
+                   o.setName('action')
+                    .setDescription('Action to perform')
+                    .setRequired(true)
+                    .addChoices(
+                        { name: 'Add Strike', value: 'add' },
+                        { name: 'Remove Strike', value: 'remove' },
+                        { name: 'Kick', value: 'kick' }
+                    ))
+               .addStringOption(o => o.setName('reason').setDescription('Reason for action').setRequired(true))
+               .addStringOption(o => o.setName('category').setDescription('Category: Strike/Termination/Blacklist').setRequired(true))
         )
         .addSubcommand(sub =>
             sub.setName('strikes')
-                .setDescription('View all strikes for a staff member')
-                .addUserOption(o => o.setName('member').setDescription('Staff member to view strikes for').setRequired(true))
+               .setDescription('View strikes for a member')
+               .addUserOption(o => o.setName('member').setDescription('Member to view').setRequired(true))
         )
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .toJSON()
 );
 
@@ -123,19 +108,21 @@ commands.push(
 commands.push(
     new SlashCommandBuilder()
         .setName('rep')
-        .setDescription('Rep management')
+        .setDescription('Rep requests')
         .addSubcommand(sub =>
             sub.setName('request')
-                .setDescription('Request a new rep for an alliance')
-                .addIntegerOption(o => o.setName('num_reps').setDescription('Number of reps requested'))
-                .addStringOption(o => o.setName('discord_link').setDescription('Discord link for the alliance'))
-                .addStringOption(o => o.setName('roblox_link').setDescription('Roblox link for the alliance'))
-                .addStringOption(o => o.setName('alliance_link').setDescription('Alliance website or group link'))
+               .setDescription('Request a new rep')
+               .addIntegerOption(o => o.setName('num_reps').setDescription('Number of reps').setRequired(true))
+               .addStringOption(o => o.setName('discord_link').setDescription('Discord link').setRequired(true))
+               .addStringOption(o => o.setName('roblox_link').setDescription('Roblox link').setRequired(true))
+               .addStringOption(o => o.setName('alliance_link').setDescription('Alliance link').setRequired(true))
         )
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .toJSON()
 );
 
+/* =======================
+   DEPLOY
+======================= */
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
@@ -144,7 +131,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands }
         );
-        console.log('✅ Slash commands deployed successfully');
+        console.log('✅ Slash commands deployed successfully.');
     } catch (err) {
         console.error('❌ Error deploying commands:', err);
     }
